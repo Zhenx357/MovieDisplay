37322020c240e406c361f71e7df3d572d81f3c2e
Zhenx357 <117629446+Zhenx357@users.noreply.github.com>
2024-06-01 02:21:47 +0200

    refactor api functions

--- movie-app/src/api/api.js
+++ movie-app/src/api/api.js
@@ -1,30 +1,38 @@
 const API_BASE_URL = 'https://feed.entertainment.tv.theplatform.eu/f/jGxigC/bb-all-pas';
 
-// Get all movies by the genre
-export const fetchMoviesByGenre = async (genre) => {
-  const response = await fetch(`${API_BASE_URL}?form=json&byTags=genre:${genre}&byProgramType=movie`);
+// Function to fetch data from the API
+const fetchData = async (url) => {
+  const response = await fetch(url);
   if (!response.ok) {
     throw new Error('Network response was not ok');
   }
-  const data = await response.json();
-  const movies = data.entries.map(entry => {
-    console.log(`Entry for ${genre}:`, entry); // Log each entry
-    const coverUrl = entry['plprogram$thumbnails']?.['orig-460x320']?.['plprogram$url'] || 'https://via.placeholder.com/150';
-    
-    // Log whether the cover URL was found or a placeholder is being used
-    if (coverUrl === 'https://via.placeholder.com/150') {
-      console.log(`Using placeholder for entry ID ${entry.id}`);
-    } else {
-      console.log(`Found cover URL for entry ID ${entry.id}: ${coverUrl}`);
-    }
-    return {
-      id: entry.id,
-      title: entry.title,
-      cover: coverUrl
-    };
-  });
+  return await response.json();
+};
+
+// Get all movies by the genre
+export const fetchMoviesByGenre = async (genre) => {
+  const data = await fetchData(`${API_BASE_URL}?form=json&byTags=genre:${genre}&byProgramType=movie`);
+  const movies = mapData(data);
   return {
     count: movies.length,
     movies: movies,
-  };
+  }
+}
+
+// Function to fetch the thumbnail
+const fetchThumbnail = (entry) => {
+  const coverUrl = entry['plprogram$thumbnails']?.['orig-460x320']?.['plprogram$url'] || 'https://via.placeholder.com/150';
+  return coverUrl;
 };
+
+// Function to map the data to the desired format
+const mapData = (data) => {
+  return data.entries.map(entry => {
+    const coverUrl = fetchThumbnail(entry);
+    return {
+      cover: coverUrl,
+      title: entry.title,
+    };
+  });
+};
+
